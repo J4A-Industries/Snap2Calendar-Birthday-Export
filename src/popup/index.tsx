13d47f86efc809aad5255d842e53c1eddf5f20fb
James <james@arnott.tech>
2023-09-26 15:16:38 +0100

    refactor: using new GA

--- src/popup/index.tsx
+++ src/popup/index.tsx
@@ -1,17 +1,15 @@
 /* eslint-disable react/jsx-no-useless-fragment */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 import React, { useEffect } from 'react';
-import 'https://www.googletagmanager.com/gtag/js?id=$PLASMO_PUBLIC_GTAG_ID';
+import { AnalyticsEvent } from '@/misc/GA';
 
 const IndexPopup = () => {
   useEffect(() => {
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = function gtag() {
-      window.dataLayer.push(arguments) // eslint-disable-line
-    };
-    window.gtag('config', process.env.PLASMO_PUBLIC_GTAG_ID, {
-      page_path: '/popup',
-    });
+    AnalyticsEvent([
+      {
+        name: 'icon_click',
+      },
+    ]);
     chrome.tabs.create({ url: chrome.runtime.getURL('tabs/main.html') });
   }, []);
 
